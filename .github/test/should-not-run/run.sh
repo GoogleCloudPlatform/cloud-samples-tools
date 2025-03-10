8d83a169ed8bb5db978f4871b07c84b6cb697d20
David Cavazos <dcavazos@google.com>
2025-03-10 16:51:03 -0700

    test failure

--- .github/test/should-not-run/run.sh
+++ .github/test/should-not-run/run.sh
@@ -12,4 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-echo "✅ Success"
+echo "❌ This should not run."
+echo "test-workflows.yaml runs only on .github/test/pkg1."
+echo "Something went wrong and all packages were run."
+exit 1
